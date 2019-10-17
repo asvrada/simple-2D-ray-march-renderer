1e49d502109a93cb80a1ab86831aa41d87d2131a
Zijie Wu <noojeff@gmail.com>
2019-10-17 00:05:19 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
     <div id="app">
-        <h1>2D Ray March render with reflection & refraction</h1>
+        <h1>2D Ray March render</h1>
         <h2>Please use Chrome. Somehow its faster.</h2>
-        <p>In this demo, it will draw the Aperture Science Logo from the game Portal:</p>
+        <p>In this demo, refraction and reflection is disabled. It will draw the Aperture Science Logo from the game Portal:</p>
         <img src="./assets/as.png" alt="">
         <ray-canvas></ray-canvas>
     </div>
